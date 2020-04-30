4088128ac51a96a75972d6745e60d4c868a63ee5
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-04-30 13:40:12 -0500

    added colors to warns/watches

--- main.py
+++ main.py
@@ -19,12 +19,13 @@ def get_url(state):
 def display_alert(alert):
     print('\n\n--------------------------------------------------')
     print(f"{alert['properties']['severity']}")
+    print('\033[31m')
     print(f"{alert['properties']['event']}")
+    print('\033[0m')
     print(f"\nHeadline:\n{alert['properties']['headline']}")
     print(f"\nDescription:\n{alert['properties']['description']}")
     print(f"\nArea:\n{alert['properties']['areaDesc']}")
     print(f'Last reload at {now}')
-    #print('\n\n--------------------------------------------------')
 
 sleep_time = int(input("How long between checks(300 = 5 min): "))
 state = input("What state are we watching(all caps abbr): ")
@@ -38,12 +39,6 @@ while (True):
     alert_data = response.json()
 
     alert_data_ids = alert_data['features']
-    # alert_data_counties = alert_data["features"][0]['properties']['areaDesc']
-    # event = alert_data['features'][0]['properties']['event']
-    # headline = alert_data['features'][0]['properties']['headline']
-    # description = alert_data['features'][0]['properties']['description']
-    # instruction = alert_data['features'][0]['properties']['instruction']
-    # secerity = alert_data['features'][0]['properties']['severity']
 
     _ = system('cls') # clears the screen for new updated info
     print(f'Last reload at {now}, Showing Warnings/Watches for {state}')
@@ -63,15 +58,6 @@ while (True):
 
         if (alert['properties']['severity'] == 'Extreme'):
             display_alert(alert)
-
-    # Displays info on screen
-    # _ = system('cls') # clears the screen for new updated info
-    # _ = system('COLOR 1') # turn text blue?
-    # print(f'Last reload at {now}')
-    # print(f'\n{headline}')
-    # print(f'\n{description}')
-    # print(f'\n{instruction}')
-    # print('\n\nControl + C to quit')
 
     # How long till recheck
     time.sleep(sleep_time)
