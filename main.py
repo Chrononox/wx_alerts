73678a1e62d4e1b26a1a27b773ea45c9c8e42c3d
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-04-30 15:01:25 -0500

    updates n shuch

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import requests
 #import datetime
 import time
 from os import system, name
+import winsound
 
 # TODO:  ADD COUNTY FILTER FOR HOME SETTING, CLEAN UP CODE, ADD DEFAULT VALUES
 
+def play_sound(frequency = 2500, duration = 1000):
+    winsound.Beep(frequency, duration)
 
 def get_url(state):
     temp = (f'https://api.weather.gov/alerts/active/area/{state}')
@@ -16,7 +19,7 @@ def get_url(state):
 def display_alert(alert, color):
     print('\n\n--------------------------------------------------')
     print(f"{alert['properties']['severity']}")
-    print('{color}')
+    print(f'{color}')
     print(f"{alert['properties']['event']}")
     print('\033[0m')
     print(f"\nHeadline:\n{alert['properties']['headline']}")
@@ -29,7 +32,7 @@ def display_alert(alert, color):
     print(f'Last reload at {now}')
 
 sleep_time = int(input("How long between checks(300 = 5 min): "))
-state = input("What state are we watching(all caps abbr): ")
+state = input("What state are we watching(all caps abbr): ").upper()
 
 ### Main Loop ###
 while (True):
@@ -46,20 +49,25 @@ while (True):
 
     for alert in alert_data_ids:
         if (alert['properties']['event'] == 'Severe Thunderstorm Warning'):
+            play_sound(1000, 500)
             display_alert(alert, '\033[31m')
         
         if (alert['properties']['event'] == 'Severe Thunderstorm Watch'):
             display_alert(alert, '\033[33m')
             
         if (alert['properties']['event'] == 'Tornado Warning'):
+            play_sound()
             display_alert(alert, '\033[95m')
 
         if (alert['properties']['event'] == 'Tornado Watch'):
+            play_sound(200, 500)
             display_alert(alert)
 
-        if (alert['properties']['severity'] == 'Extreme'):
-            display_alert(alert, '\033[36m')
+        # if (alert['properties']['severity'] == 'Extreme'):
+        #     play_sound()
+        #     display_alert(alert, '\033[36m')
 
     # How long till recheck
+    
     time.sleep(sleep_time)
 
