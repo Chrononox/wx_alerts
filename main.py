12976130f00fba66e725d272d6abba29e3d457b3
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-04-30 10:33:21 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+#grab and display weather alerts
+
+import json
+import requests
+import datetime
+from os import system, name
+
+# TODO: ADD COLORS FOR ALERTS, CLEAN UP CODE
+
+def get_url(state):
+    temp = (f'https://api.weather.gov/alerts/active/area/{state}')
+    return temp
+
+# url ='https://api.weather.gov/alerts/MO'
+# url_KS = 'https://api.weather.gov/alerts/active/area/KS'
+# url_MO = 'https://api.weather.gov/alerts/active/area/MO'
+# url_CO = 'https://api.weather.gov/alerts/active/area/CO'
+
+sleep_time = 300 # in seconds
+
+sleep_time = int(input("How long between checks(300 = 5 min): "))
+state = input("What state are we watching(all caps abbr): ")
+
+while (True):
+    now = datetime.datetime.now().time()
+    # Gets info and stores it into variables
+    response = requests.get(get_url(state))
+    
+    alert_data = response.json()
+    alert_data_counties = alert_data["features"][0]['properties']['areaDesc']
+    event = alert_data['features'][0]['properties']['event']
+    headline = alert_data['features'][0]['properties']['headline']
+    description = alert_data['features'][0]['properties']['description']
+    instruction = alert_data['features'][0]['properties']['instruction']
+
+    # Displays info on screen
+    _ = system('cls') # clears the screen for new updated info
+    print(f'Last reload at {now}')
+    print(f'\n{headline}')
+    print(description)
+    print(instruction)
+    print('\n\nControl + C to quit')
+
+    # How long till recheck
+    time.sleep(sleep_time)
+
