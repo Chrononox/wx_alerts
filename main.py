e6716cfa53ef1e3be9f448a798d81783e616a3e3
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-04-30 13:12:49 -0500

    updates

--- main.py
+++ main.py
@@ -8,48 +8,61 @@ from os import system, name
 
 # TODO: ADD COLORS FOR ALERTS, ADD COUNTY FILTER FOR HOME SETTING, CLEAN UP CODE,
 #        ADD AUTO CAP FOR STATS, ADD DEFAULT VALUES, FILTER ALERT TYPES
+#
 # NOTE: For each seperate alert is a new id, need to cycle through them
+#       Maybe show all alerts unless watched area is found then only show watched area?
 
 def get_url(state):
     temp = (f'https://api.weather.gov/alerts/active/area/{state}')
     return temp
 
-# url ='https://api.weather.gov/alerts/MO'
-# url_KS = 'https://api.weather.gov/alerts/active/area/KS'
-# url_MO = 'https://api.weather.gov/alerts/active/area/MO'
-# url_CO = 'https://api.weather.gov/alerts/active/area/CO'
+def display_alert(alert):
+    print('\n\n--------------------------------------------------')
+    print(f"{alert['properties']['severity']}")
+    print(f"{alert['properties']['event']}")
+    print(f"\nHeadline:\n{alert['properties']['headline']}")
+    print(f"\nDescription:\n{alert['properties']['description']}")
+    print(f"\nArea:\n{alert['properties']['areaDesc']}")
+    print(f'Last reload at {now}')
+    #print('\n\n--------------------------------------------------')
 
-sleep_time = 300 # in seconds
 sleep_time = int(input("How long between checks(300 = 5 min): "))
 state = input("What state are we watching(all caps abbr): ")
 
+### Main Loop ###
 while (True):
     now = time.asctime(time.localtime(time.time()))
+
     # Gets info and stores it into variables
     response = requests.get(get_url(state))    
     alert_data = response.json()
 
     alert_data_ids = alert_data['features']
-    alert_data_counties = alert_data["features"][0]['properties']['areaDesc']
-    event = alert_data['features'][0]['properties']['event']
-    headline = alert_data['features'][0]['properties']['headline']
-    description = alert_data['features'][0]['properties']['description']
-    instruction = alert_data['features'][0]['properties']['instruction']
-    secerity = alert_data['features'][0]['properties']['severity']
+    # alert_data_counties = alert_data["features"][0]['properties']['areaDesc']
+    # event = alert_data['features'][0]['properties']['event']
+    # headline = alert_data['features'][0]['properties']['headline']
+    # description = alert_data['features'][0]['properties']['description']
+    # instruction = alert_data['features'][0]['properties']['instruction']
+    # secerity = alert_data['features'][0]['properties']['severity']
 
     _ = system('cls') # clears the screen for new updated info
-    print(f'Last reload at {now}')
+    print(f'Last reload at {now}, Showing Warnings/Watches for {state}')
+
     for alert in alert_data_ids:
-        print('\n\n--------------------------------------------------')
-        print(f"\nHeadline:\n{alert['properties']['headline']}")
-        print(f"\nDescription:\n{alert['properties']['description']}")
-        print(f"\nArea:\n{alert['properties']['areaDesc']}")
-        if('Jackson, MO' in alert['properties']['areaDesc']): # this will be used to find home county alerts
-            print("\n\nCOUNTY FOUND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n")
-        print(f'Last reload at {now}')
-        print('\n\n--------------------------------------------------')
+        if (alert['properties']['event'] == 'Severe Thunderstorm Warning'):
+            display_alert(alert)
+        
+        if (alert['properties']['event'] == 'Severe Thunderstorm Watch'):
+            display_alert(alert)
+            
+        if (alert['properties']['event'] == 'Tornado Warning'):
+            display_alert(alert)
 
+        if (alert['properties']['event'] == 'Tornado Watch'):
+            display_alert(alert)
 
+        if (alert['properties']['severity'] == 'Extreme'):
+            display_alert(alert)
 
     # Displays info on screen
     # _ = system('cls') # clears the screen for new updated info
