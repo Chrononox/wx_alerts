5df8737c663cfd3195efb5ededcf2a956bcd4947
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-05-13 10:35:12 -0500

    clean up

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 #grab and display weather alerts
 
-# TODO: Allow nationwide or per state (?multi-state?) -- This is now all automatic
-#       Remember active seen alerts to avoid re-beeps
-#       Need to make sure special weather statemens are getting thouugh
-
 # Get active alerts nation wide = 'https://api.weather.gov/alerts/active?status=actual&message_type=alert&region_type=land'
 # Get active alerts per state   = 'https://api.weather.gov/alerts/active/area/{state}'
-
 
 # Imports
 import json
@@ -14,7 +9,6 @@ import requests
 import time
 from os import system, name
 import winsound
-
 
 # Variables
 counter = 0
@@ -34,14 +28,13 @@ ww = 'High Wind Warning'
 eh = 'Excessive Heat Warning'
 fire = 'Fire Weather Warning'
 sws = 'Special Weather Statement'
-sws = 'removed for test'
-
 
 twarn_color = '\033[95m'
 twatch_color = '\033[96m'
 swarn_color = '\033[31m'
 swatch_color = '\033[33m'
 
+# Vars used for testing only
 tor_test = {
     "properties":{
         "areaDesc": "Lee's Summit",
@@ -68,7 +61,7 @@ city = "Lee's Summit"
 county = "Jackson, MO"
 state = "MO"
 
-## FUNCTIONS ##
+#### FUNCTIONS ####
 
 # Plays sounds
 def play_sound(frequency = 2500, duration = 1000):
@@ -100,7 +93,6 @@ def display_alert(alert, color = '\033[0m'):
 def effects(alert):
              
     if (alert['properties']['event'] == 'Tornado Warning'):
-        #if(current_alerts == alert['properties']['id'])
         play_sound(2400,1000)
         display_alert(alert, twarn_color)
 
@@ -137,8 +129,7 @@ def debug_mode():
         print('\nprint - print alert') 
         print("\nset -state")
 
-
-## Start up ##
+##@@ Start up @@##
 _ = system('cls') # clears the screen for new updated info
 prompt = input("Weather alerts v2. enter to continue:")
 
@@ -146,7 +137,7 @@ if(prompt.lower() == 'debug'):
     run = False
     debug_mode()
 
-#!! Main Loop !!#
+##!! Main Loop !!##
 while(run):
 
     _ = system('cls') # clears the screen for new updated info
@@ -194,7 +185,7 @@ while(run):
                 for alert in alert_data_ids:
                     thing  = alert['properties']['event']
                     #if (thing == tw or thing == twa or thing == sw or thing == swa or thing == fw or thing == ww or thing == eh or thing == fire or thing == sws):
-                    if (thing == tw or thing == twa or thing == sw or thing == swa or thing == sws):
+                    if (thing == tw or thing == twa or thing == sw or thing == swa):
                         current_alerts.append(alert['id'])
                         all_alert = True
                         effects(alert)
@@ -214,5 +205,8 @@ while(run):
 
     # Reset alerts
     local_alert = False
+    state_alert = False
+    all_alert = False
+    
     # Reccheck Timer
     time.sleep(sleep_time)
