8ca6893cca113abe4405574244335e2f6e24ea80
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-05-19 18:19:42 -0500

    workin the window

--- display.py
+++ display.py
@@ -10,14 +10,17 @@ def test_window():
     alert_data_ids = alert_data['features']
     window=tk.Tk()
     window.title("Alert Window")
+    
 
     for alert in alert_data_ids:
-        label = tk.Label(text=alert['properties']['event'])
-        lbl_severity = tk.Label(text=alert['properties']['severity'])
-        lbl_headline = tk.Label(text=alert['properties']['headline'])
+        frame = tk.Frame(master=window)
+        label = tk.Label(master=frame, text=alert['properties']['event'])
+        lbl_severity = tk.Label(master=frame, text=alert['properties']['severity'])
+        lbl_headline = tk.Label(master=frame, text=alert['properties']['headline'])
         lbl_severity.pack()
         label.pack()
         lbl_headline.pack()
+        frame.pack()
 
     label2 = tk.Label(text="^.^")
     label2.pack()
