e18e70f97a5265b93562329bfe89aa66beea38d4
Chrononox <59702002+Chrononox@users.noreply.github.com>
2020-06-19 23:46:40 -0500

    stuff

--- new.py
+++ new.py
@@ -1,7 +1,6 @@
 
 
-
-
+#~~ FUNCTIONS ~~ #
 def get_alerts_us():
     '''Returns the alerts for the whole US'''
     temp = (f'https://api.weather.gov/alerts/active?status=actual&message_type=alert&region_type=land')
